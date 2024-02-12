dc85b661bc703f072c31d9bdc206a1d10b608e0c
atul <atul.etech2011@gmail.com>
2024-02-12 17:54:05 +0530

    implemented get all requirements and accepted requirements api

--- src/api/routes/requirement.js
+++ src/api/routes/requirement.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import * as requirementController from '../controllers/requirement.controller.js';
-import { verifyUser } from '../../middlewares/verifyUser.js';
+import { isAdmin, verifyUser } from '../../middlewares/verifyUser.js';
 
 const requirementRouter = express.Router();
 requirementRouter.post('/set',verifyUser, requirementController.setRequirement);
 requirementRouter.get('/get/:id',verifyUser, requirementController.getRequirement);
-requirementRouter.get('/get_all',verifyUser, requirementController.getRequirements);
+requirementRouter.get('/get_all',verifyUser, requirementController.getPendingRequirements);
 requirementRouter.put('/accept/:id',verifyUser, requirementController.acceptRequirement);
+requirementRouter.get('/accepted',verifyUser, requirementController.getAcceptedRequirements);
+requirementRouter.get('/all/fetch',verifyUser,isAdmin, requirementController.getAllRequirements);
+
 
 export default requirementRouter;
